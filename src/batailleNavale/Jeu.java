0641ffc5f87ba2333acdf1043b9e88f3443efba9
poignanj <33960552+poignanj@users.noreply.github.com>
2018-09-27 10:47:21 -0400

    UML
    UML final - peut être sujet à une modification, pour l'interface PlacerNavire.
    L'implémentation PlacerNavireUI n'existe pas dans le code, elle est donnée à titre d'exemple pour l'évolution possible du soft.
    
    détail : renomage de variable pour coller au schéma de classes.

--- src/batailleNavale/Jeu.java
+++ src/batailleNavale/Jeu.java
@@ -3,13 +3,13 @@ package batailleNavale;
 import outils.Coordonnee;
 
 public class Jeu {
-	private Carte c1;
-	private Carte c2;
+	private Carte joueur1;
+	private Carte joueur2;
 
 	public Jeu() {
-		c1 = new Carte(new PlacerNavireConsole(), new SelectionnerNavireConsole(), new FaireFeuConsole(),
+		joueur1 = new Carte(new PlacerNavireConsole(), new SelectionnerNavireConsole(), new FaireFeuConsole(),
 				new DeplacerNavireConsole());
-		c2 = new Carte(new PlacerNavireConsole(), new SelectionnerNavireConsole(), new FaireFeuConsole(),
+		joueur2 = new Carte(new PlacerNavireConsole(), new SelectionnerNavireConsole(), new FaireFeuConsole(),
 				new DeplacerNavireConsole());
 		this.tourDeJeu();
 	}
@@ -17,12 +17,12 @@ public class Jeu {
 	private void tourDeJeu() {
 		Coordonnee c = null;
 		while (true) {
-			c = c1.tirer(c);
+			c = joueur1.tirer(c);
 			if (c == null) {
 				System.out.println("Joueur 2 remporte la partie !!!");
 				break;
 			}
-			c = c2.tirer(c);
+			c = joueur2.tirer(c);
 			if (c == null) {
 				System.out.println("Joueur 1 remporte la partie !!!");
 				break;
