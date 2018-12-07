2593794a504db8642c9eb480d3cecfe5992150e6
unknown <vivien.piris@live.fr>
2018-12-06 22:20:14 -0500

    modif singleton pour client

--- Projet_cartes/src/lo02/Serveur/Client.java
+++ Projet_cartes/src/lo02/Serveur/Client.java
@@ -8,26 +8,42 @@ public class Client  {
 
    private  int port = 8080;
    private Socket socket;
-   private final String host;
+   private String host;
    private static Client instance = null;
-   public Client(String host, int port ) {
+   private Client(String host, int port ) {
      
       this.host=host;
       this.port=port;
    }
    
-   public Client(String host ) {
+   private Client(String host ) {
 	     
 	   this.host=host;
-	   }
+   }
+   private Client( ) {
+	     
+	   this.host= "127.0.0.1";
+	   this.port = 8080;
+   }
 
-   public Client start() {
+   public Client start(String pHost, int pPort) {
+	   this.host=pHost;
+	   this.port=pPort;
       try {
     	  socket = new Socket(InetAddress.getByName(host), port);
       } catch (IOException e) {
       }
       return this;
    }
+   
+   public Client start(String pHost) {
+	   this.host=pHost;
+	      try {
+	    	  socket = new Socket(InetAddress.getByName(host), port);
+	      } catch (IOException e) {
+	      }
+	      return this;
+	   }
 
    private void close() {
      
@@ -48,38 +64,19 @@ public class Client  {
   	 * @param pHost adresse du serveur
   	 * @return Client renvoie l'instance du Client
   	 */
-   public static Client Instance(String pHost)
+   public static Client Instance()
    {   
        if (instance == null)
        {   
            synchronized(Client.class)
            {
                if (instance == null)
-               {   instance = new Client(pHost);
+               {   instance = new Client();
                }
            }
        }
        return instance;
    }
-  /**
-	 *  Instance du client 
-	 * @param pHost adresse du serveur
-	 * @param pPort port du serveur
-	 * @return Client renvoie l'instance du Client
-	 */
-  public static Client Instance(String pHost, int pPort)
-   {   
-       if (instance == null)
-       {   
-           synchronized(Client.class)
-           {
-               if (instance == null)
-               {   instance = new Client(pHost, pPort);
-               }
-           }
-       }
-       return instance;
-   }
-
 }
+
 
